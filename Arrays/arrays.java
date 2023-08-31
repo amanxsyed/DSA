61efb973ca53eee30a3f51961ad40e55a8e2b651
Syed <amanxsyed@gmail.com>
2023-09-01 01:01:22 +0500

    1 simple array program in array.java

---
+++ Arrays/arrays.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class arrays {
+    public static void main (String[] args){
+        int[] marks = new int[3];
+        marks[0]=97; //phy
+        marks[1]=98; //chem
+        marks[2]=95; //eng
+        for(int i=0; i<3; i++){
+            System.out.println(marks[i]);   
+        }
+    }
+}
