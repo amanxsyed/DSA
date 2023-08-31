9dc4625b73f322a9b6283d92041bfff89b15b50c
Syed <amanxsyed@gmail.com>
2023-09-01 02:35:38 +0500

    Array Program to find Number Position

---
+++ Arrays/FindNumPositionInArray.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/* Question
+ * Take an Array as input from the user. 
+ * Search for a given number x && print the index at which   it occurs
+ */
+public class FindNumPositionInArray{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in); // Using Scanner class for user input
+        System.out.println("Size of an Array?");
+        int size = sc.nextInt(); 
+        int numbers[] = new int[size]; // Declaring an array
+
+        for(int i=0; i<size; i++){
+            numbers[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the number to find: ");
+
+        int num = sc.nextInt(); //input for number user wanted to find?
+
+        for(int i=0; i<numbers.length; i++){
+            if(numbers[i]==num){
+                System.out.println("Number found at position: "+ i);
+                break;
+            }else{
+                System.out.println("Number not found");
+                break;
+            }
+        }
+
+    }   
+}
