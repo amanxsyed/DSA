c52ffe6746e8a3911dadf59170f2158bc1f394b5
Syed <amanxsyed@gmail.com>
2023-09-03 01:50:49 +0500

    add var numberOfNames for maintaining the code

--- Arrays/NamesArray.java
+++ Arrays/NamesArray.java
@@ -5,24 +5,29 @@ public class NamesArray{
         
         //Using Scanner Class for Input Values From User
         Scanner sc = new Scanner(System.in);
-        System.out.println("How many names you wanted to enter: ");
-        int size = sc.nextInt();
+        int size = 100;
 
         //Declaring an Array
         String[] names = new String[size]; 
 
-        System.out.println("Enter the Names You Want to Enter: ");
+        System.out.println("How Many Names You Want to Enter: ");
+        int numberOfNames= sc.nextInt();
+
+        System.out.println("Enter Names: ");
 
         //input
-        for(int i=0; i<size; i++){
+        for(int i=0; i<numberOfNames; i++){
             names[i] = sc.next();
         }
 
         //output
-        for(int i=0; i<names.length; i++){
-            System.out.println("name "+ (i+1) +" is : " + names[i]);
+        for(int i=0; i<numberOfNames; i++){
+            System.out.println("Name on position 2"+ (i+1) +" is : " + names[i]);
         }
 
+        System.out.println("Your Names Printed Successfully");
+        System.out.println("Thanks for using our program!");
+        System.out.println("Happy Coding");
 
     }
 }
