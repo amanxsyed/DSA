fa6e76244939239cca0afe4a0f10d799bd55ae26
Syed <amanxsyed@gmail.com>
2023-09-03 01:58:09 +0500

    Declare an Array of Numbers

---
+++ Arrays/Max_Min_Num_Array.java
@@ -0,0 +1,8 @@
+public class Max_Min_Num_Array{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] numbers = new int[size];
+        
+    }
+}
