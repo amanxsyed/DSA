01546c89582171e977aaeda8b9e4b4cbc71db1a1
Syed <amanxsyed@gmail.com>
2023-09-03 23:19:33 +0500

    Program Completed Check it out

--- Arrays/Max_Min_Num_Array.java
+++ Arrays/Max_Min_Num_Array.java
@@ -1,8 +1,36 @@
+import java.util.*;
 public class Max_Min_Num_Array{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("How Many Number You Wanted to Add in Test ? ");
         int size = sc.nextInt();
+
+        //Declare an Array
         int[] numbers = new int[size];
-        
+
+        //input Loop
+        for(int i=0; i<size; i++){
+            System.out.println("Enter Number "+(i+1)+" : ");
+            numbers[i] = sc.nextInt();
+        }
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for(int i=0; i<numbers.length;i++){
+            if(numbers[i] < min ){
+                min = numbers[i];
+                
+            }
+        }
+
+        for(int i =0;i<numbers.length;i++){
+            if(numbers[i] > max){
+                max = numbers[i];
+                
+            }
+        }
+        System.out.println("Smallest Number is: "+ min);
+        System.out.println("Largest Numbers is: "+max);
     }
 }
