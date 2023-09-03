a15836d14229f3ef661555c2ad1452052b21742b
Syed <amanxsyed@gmail.com>
2023-09-04 00:32:02 +0500

    Use  Methods & Objects in Code to look more cleaner && understandable

--- Arrays/Max_Min_Num_Array.java
+++ Arrays/Max_Min_Num_Array.java
@@ -1,13 +1,36 @@
 import java.util.*;
 // Find the maximum & minimum number in an array of integers. 
+ 
 public class Max_Min_Num_Array{
+
+    //Declaring a Method for Max Value
+    public int getMax(int numbers[], int n){
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            if(numbers[i]>max){
+                max=numbers[i];
+            }
+        }
+        return max;
+    }
+
+    //Declaring a Method for Min Value
+    public int getMin(int numbers[], int n){
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            if(numbers[i]<min){
+                min=numbers[i];
+            }
+        }
+        return min;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("How Many Number You Wanted to Add in Test ? ");
         int size = sc.nextInt();
 
         //Declare an Array
-        int[] numbers = new int[size];
+        int[] numbers = new int[100];
 
         //input Loop
         for(int i=0; i<size; i++){
@@ -15,25 +38,11 @@ public class Max_Min_Num_Array{
             numbers[i] = sc.nextInt();
         }
 
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
+        Max_Min_Num_Array obj = new Max_Min_Num_Array();
+        int min = obj.getMin(numbers, size);
+        int max = obj.getMax(numbers, size);
 
-        //Finding Min Value in Array
-        for(int i=0; i<numbers.length;i++){
-            if(numbers[i] < min ){
-                min = numbers[i];
-                
-            }
-        }
-
-        //Finding Max Value in Array
-        for(int i =0;i<numbers.length;i++){
-            if(numbers[i] > max){
-                max = numbers[i];
-                
-            }
-        }
         System.out.println("Smallest Number is: "+ min);
-        System.out.println("Largest Numbers is: "+max);
+        System.out.println("Largest Numbers is: "+ max);
     }
 }
