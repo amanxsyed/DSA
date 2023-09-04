239613f5413cf992b34b7f4733bf73327942c714
Syed <amanxsyed@gmail.com>
2023-09-05 00:06:12 +0500

    By using method & XOR BitWise operator completed the Question

---
+++ Arrays/Find_Unique.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+/* Question
+ * you have been given an integer array of size N. Where N is equal to [2M+1]. In given array list, M Number  is presented twice & one number is presented only once. You need to find and return that number which  is unique in array. solve this question in Java
+ */
+
+public class Find_Unique {
+    public static int FindUni(int[] arr){
+        int result = 0;
+        for(int num : arr){
+            result ^= num;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,1,2,3};
+        System.out.println(FindUni(arr));
+        
+    }
+}
